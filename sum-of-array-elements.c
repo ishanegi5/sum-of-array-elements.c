6d68229af31cc5259509dfb1951370b92d5cb2bf
Isha Negi <itsishanegi@gmail.com>
2024-10-13 00:02:04 +0530

    Update sum-of-array-elements.c

--- sum-of-array-elements.c
+++ sum-of-array-elements.c
@@ -5,7 +5,7 @@ printf("Enter 5 numbers: ");
 for(int i=0;i<5;i++){
     scanf("%d",&arr[i]);
 }
-printf("5 numbers array are: ");
+printf("sum of 5 numbers in array are: ");
 for(int j=0;j<5;j++){
    sum+=arr[j];
     
